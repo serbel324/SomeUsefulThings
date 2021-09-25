c422ba6807ba00584f6112ad8bba50b25db02603
Radiant32 <serbel324@gmail.com>
2021-09-25 18:15:37 +0300

    Added Random.h

---
+++ SomeUsefulThings/Random.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <ctime>
+#include <random>
+
+double get_random_double(double a, double b)
+{
+    if (b <= a) {
+        return 0;
+    }
+
+    static std::mt19937_64 randomizer(std::time(0));
+    long double base = randomizer(); /* in range [0; 9981545732273789042) */
+    long double maxr = 9981545732273789042.;
+    long double normilized = base / maxr; /* in range [0; 9981545732273789042) */
+    return normilized * (b - a) + a;
+}
+
+int get_random_int(int a, int b)
+{
+    if (b <= a) {
+        return 0;
+    }
+
+    static std::mt19937_64 randomizer(std::time(0));
+    unsigned long long base = randomizer(); /* in range [0; 9981545732273789042) */
+    long long normilized = base % (b - a);
+    return normilized + a;
+}
