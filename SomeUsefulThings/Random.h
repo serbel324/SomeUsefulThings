31d6bdb105dc4e7fb3ad1a0193fe5a81fc564e77
Radiant32 <serbel324@gmail.com>
2021-09-25 18:48:28 +0300

    Random fixed

--- SomeUsefulThings/Random.h
+++ SomeUsefulThings/Random.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <climits>
 #include <ctime>
 #include <random>
 
@@ -11,9 +12,9 @@ double get_random_double(double a, double b)
     }
 
     static std::mt19937_64 randomizer(std::time(0));
-    long double base = randomizer(); /* in range [0; 9981545732273789042) */
-    long double maxr = 9981545732273789042.;
-    long double normilized = base / maxr; /* in range [0; 9981545732273789042) */
+    long double base = randomizer(); /* in range [0; ULLONG_MAX) */
+    long double maxr = ULLONG_MAX;
+    long double normilized = base / maxr; /* in range [0; ULLONG_MAX) */
     return normilized * (b - a) + a;
 }
 
@@ -25,7 +26,7 @@ int get_random_int(int a, int b)
     }
 
     static std::mt19937_64 randomizer(std::time(0));
-    unsigned long long base = randomizer(); /* in range [0; 9981545732273789042) */
+    unsigned long long base = randomizer(); /* in range [0; ULLONG_MAX) */
     long long normilized = base % (b - a);
     return normilized + a;
 }
